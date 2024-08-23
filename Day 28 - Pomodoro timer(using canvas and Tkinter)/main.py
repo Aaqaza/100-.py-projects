447edb583491a51389cee00d4a631f86d551ba27
Aaqil <109215246+Aaqaza@users.noreply.github.com>
2024-08-23 16:27:48 +0530

    Update main.py

--- Day 28 - Pomodoro timer(using canvas and Tkinter)/main.py
+++ Day 28 - Pomodoro timer(using canvas and Tkinter)/main.py
@@ -31,14 +31,14 @@ def start_timer():
     global reps
     reps += 1
 
-    # work_sec = WORK_MIN * 60
-    # short_break_sec = SHORT_BREAK_MIN * 60
-    # long_break_sec = LONG_BREAK_MIN * 60
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
     # For testing
-    work_sec = 5
-    short_break_sec = 2
-    long_break_sec = 10
+    # work_sec = 5
+    # short_break_sec = 2
+    # long_break_sec = 10
 
     if reps in (1, 3, 5, 7):
         count_down(work_sec)
