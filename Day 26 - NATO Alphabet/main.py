9e539766455c3415485f739fe42887b2126159fe
Aaqil <109215246+Aaqaza@users.noreply.github.com>
2024-10-14 14:54:40 +0530

    Update main.py

--- Day 26 - NATO Alphabet/main.py
+++ Day 26 - NATO Alphabet/main.py
@@ -2,16 +2,27 @@
 #  {new_key:new_value for (index, row) in df.iterrows()}
 
 import pandas
+
 alphabets = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 # TODO 1: Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 phonetic_dict = {row.letter: row.code for (index, row) in alphabets.iterrows()}
+print(phonetic_dict)
 
-word = input("Enter your word: ").upper()
 
-# TODO 2: Create a list of the phonetic code words from a word that the user inputs.
-nato_alphabets = [phonetic_dict[letter] for letter in word]
-print(nato_alphabets)
+# TODO 2: Create a list of the phonetic code words from a word that the user inputs
+def generate_nato_words():
+    word = input("Enter your word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_nato_words()
+    else:
+        print(output_list)
+
+
+generate_nato_words()
 
 
 
